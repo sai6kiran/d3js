adfd01773dac13aaafb9a5d63fcb3a947936aa93
sai6kiran <syerrag1@hawk.iit.edu>
2018-08-13 13:12:45 -0500

    First commit

---
+++ network-data-analysis/rickshaw/pubnub/app.js
@@ -0,0 +1,41 @@
+var PUBNUB = require('pubnub');
+function publish() {
+   
+    pubnub = new PubNub({
+        publishKey : 'demo',
+        subscribeKey : 'demo'
+    })
+       
+    function publishSampleMessage() {
+        console.log("Since we're publishing on subscribe connectEvent, we're sure we'll receive the following publish.");
+        var publishConfig = {
+            channel : "hello_world",
+            message : {
+                title: "greeting",
+                description: "hello world!"
+            }
+        }
+        pubnub.publish(publishConfig, function(status, response) {
+            console.log(status, response);
+        })
+    }
+       
+    pubnub.addListener({
+        status: function(statusEvent) {
+            if (statusEvent.category === "PNConnectedCategory") {
+                publishSampleMessage();
+            }
+        },
+        message: function(msg) {
+            console.log(msg.message.title);
+            console.log(msg.message.description);
+        },
+        presence: function(presenceEvent) {
+            // handle presence
+        }
+    })      
+    console.log("Subscribing..");
+    pubnub.subscribe({
+        channels: ['hello_world'] 
+    });
+};
